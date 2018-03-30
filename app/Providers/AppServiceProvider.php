4bb4507eb4999135bbbb267de27d2b80db1f6803
Mandar <mmathure@syr.edu>
2018-03-30 18:56:29 -0400

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
+
+if ($this->app->environment() == 'production') {
+    URL::forceScheme('https');
+}
 class AppServiceProvider extends ServiceProvider
 {
     /**
