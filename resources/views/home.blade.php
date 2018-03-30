6943bdd029bd842bb11cdc72dff605fa04bd9354
Mandar <mmathure@syr.edu>
2018-03-30 02:11:42 -0400

    added images to show

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,18 +7,25 @@
 
             <hr>
             @if(count($posts)>0)
-                @foreach($posts as $post)
-            <a href="posts/{{$post->id}}" id="post"><div class="card zoom">
-                        <div class="card-body">
-                        <h5 class="card-title">{{$post->title}}</h5>
-                          <p class="card-text">written on {{$post->created_at}}</p>
-                        </div>
-                      </div>
-                    </a>
-                <br>
-            
-            
-                @endforeach
+            @foreach($posts as $post)
+      
+            <a href="posts/{{$post->id}}" id="post">
+        
+            <div class="row border zoom" >
+                <span><img  style="height: 100px;width:100px;" src="storage/cover_images/{{$post->cover_image}}" /></span>
+                  <span style="padding:20px;"><h5 class="card-title">{{$post->title}}</h5>
+                  </span>
+                  <p style="margin-right:5px;margin-bottom:0;margin-left:auto;margin-top:auto;"><small>...by {{$post->user->name}}</small></p>
+      
+                  </div>
+            </a>
+          
+      
+      
+          <br>
+      
+      
+          @endforeach
                 {{$posts->links()}}
 
                 @else
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 <br>
 <h1>{{$post->title}}</h1>
+<div class="text-center" style="height:100%;width:100%;">
+    <img  style="height:100%;width:100%;" src="/storage/cover_images/{{$post->cover_image}}">
+</div>
+<br>
 <p>{{$post->body}}</p>
 <hr>
 <small>Written by {{$post->user->name}} on {{$post->created_at}}</small>
